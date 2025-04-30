4332c90075773743b619cd6f3d6f8d4536faaeb5
KasiMKurtay <kasm.krt44@gmail.com>
2025-04-30 17:36:16 +0300

    Created user routes and configured controllers

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,3 @@
-import { error } from "console";
 import { generateTokenAndSetCookie } from "../lib/utils/generateToken.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
@@ -7,36 +6,33 @@ export const signup = async (req, res) => {
   try {
     const { fullName, username, email, password } = req.body;
 
-    const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(email)) {
       return res.status(400).json({ error: "Invalid email format" });
     }
 
     const existingUser = await User.findOne({ username });
-
     if (existingUser) {
-      return res.status(400).json({ error: "Username already exists" });
+      return res.status(400).json({ error: "Username is already taken" });
     }
 
     const existingEmail = await User.findOne({ email });
-
     if (existingEmail) {
-      return res.status(400).json({ error: "Email already exists" });
+      return res.status(400).json({ error: "Email is already taken" });
     }
 
     if (password.length < 6) {
-      res
+      return res
         .status(400)
-        .json({ error: "Password length must be at least 6 characters" });
+        .json({ error: "Password must be at least 6 characters long" });
     }
 
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
     const newUser = new User({
-      username,
       fullName,
+      username,
       email,
       password: hashedPassword,
     });
@@ -47,8 +43,8 @@ export const signup = async (req, res) => {
 
       res.status(201).json({
         _id: newUser._id,
-        username: newUser.username,
         fullName: newUser.fullName,
+        username: newUser.username,
         email: newUser.email,
         followers: newUser.followers,
         following: newUser.following,
@@ -59,30 +55,30 @@ export const signup = async (req, res) => {
       res.status(400).json({ error: "Invalid user data" });
     }
   } catch (error) {
-    console.log("Error signing up controller:", error);
-    res.status(500).json({ error: "Internal server error" });
+    console.log("Error in signup controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
 export const login = async (req, res) => {
   try {
     const { username, password } = req.body;
-
     const user = await User.findOne({ username });
     const isPasswordCorrect = await bcrypt.compare(
       password,
-      user.password || ""
+      user?.password || ""
     );
 
-    if (user && isPasswordCorrect) {
+    if (!user || !isPasswordCorrect) {
       return res.status(400).json({ error: "Invalid username or password" });
     }
 
     generateTokenAndSetCookie(user._id, res);
+
     res.status(200).json({
       _id: user._id,
-      username: user.username,
       fullName: user.fullName,
+      username: user.username,
       email: user.email,
       followers: user.followers,
       following: user.following,
@@ -90,18 +86,18 @@ export const login = async (req, res) => {
       coverImg: user.coverImg,
     });
   } catch (error) {
-    console.log("Error signing up controller:", error);
-    res.status(500).json({ error: "Internal server error" });
+    console.log("Error in login controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
 export const logout = async (req, res) => {
   try {
     res.cookie("jwt", "", { maxAge: 0 });
-    res.status(200).json({ message: "Logout successfully" });
+    res.status(200).json({ message: "Logged out successfully" });
   } catch (error) {
-    console.log("Error logout  controller:", error);
-    res.status(500).json({ error: "Internal server error" });
+    console.log("Error in logout controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
@@ -110,7 +106,7 @@ export const getMe = async (req, res) => {
     const user = await User.findById(req.user._id).select("-password");
     res.status(200).json(user);
   } catch (error) {
-    console.log("Error getMe controller:", error.message);
-    res.status(500).json({ error: "Internal server error" });
+    console.log("Error in getMe controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
