90d27400be517dbf22b632b2d74b4e1953c5c86e
KasiMKurtay <kasm.krt44@gmail.com>
2025-04-30 15:58:13 +0300

    Schema, controllers and auth Routes has been created

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,21 @@ import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
 app.listen(port, () => {
-  console.log("Server is running on port " + port);
+  console.log(`Server is running on port ${port}`);
   connectMongoDB();
 });
