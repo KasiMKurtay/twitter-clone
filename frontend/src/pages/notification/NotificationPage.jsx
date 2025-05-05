96e543ce6abb1e991de1e10b6bd11d9c1f0dbaaa
KasiMKurtay <kasm.krt44@gmail.com>
2025-05-06 01:36:02 +0300

    singleNotification and deleteAllNotification function add

--- frontend/src/pages/notification/NotificationPage.jsx
+++ frontend/src/pages/notification/NotificationPage.jsx
@@ -4,32 +4,77 @@ import LoadingSpinner from "../../components/common/LoadingSpinner";
 import { IoSettingsOutline } from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
 import { FaHeart } from "react-icons/fa6";
+import { FaTrashAlt } from "react-icons/fa"; // Çöp kutusu ikonu
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { toast } from "react-hot-toast";
 
 const NotificationPage = () => {
-  const isLoading = false;
-  const notifications = [
-    {
-      _id: "1",
-      from: {
-        _id: "1",
-        username: "kasimkurtay1",
-        profileImg: "/avatars/boy2.png",
-      },
-      type: "follow",
-    },
-    {
-      _id: "2",
-      from: {
-        _id: "2",
-        username: "kasimkurtay2",
-        profileImg: "/avatars/girl1.png",
-      },
-      type: "like",
-    },
-  ];
+  const queryClient = useQueryClient();
 
-  const deleteNotifications = () => {
-    alert("All notifications deleted");
+  const { data: notifications, isLoading } = useQuery({
+    queryKey: ["notifications"],
+    queryFn: async () => {
+      try {
+        const res = await fetch(`/api/notifications`, {
+          method: "GET",
+        });
+        const data = await res.json();
+        if (!res.ok) {
+          throw new Error(data.error || "Something went wrong");
+        }
+        return data ?? null;
+      } catch (error) {
+        throw new Error(error.message);
+      }
+    },
+  });
+
+  const { mutate: deleteNotifications } = useMutation({
+    mutationFn: async () => {
+      try {
+        const res = await fetch(`/api/notifications`, {
+          method: "DELETE",
+        });
+        const data = await res.json();
+        if (!res.ok) {
+          throw new Error(data.error || "Something went wrong");
+        }
+      } catch (error) {
+        throw new Error(error.message);
+      }
+    },
+    onSuccess: () => {
+      toast.success("Notifications deleted successfully");
+      queryClient.invalidateQueries({ queryKey: ["notifications"] });
+    },
+  });
+
+  const { mutate: singleNotification } = useMutation({
+    mutationFn: async (id) => {
+      try {
+        const res = await fetch(`/api/notifications/${id}`, {
+          method: "DELETE",
+        });
+        const data = await res.json();
+        if (!res.ok) {
+          throw new Error(data.error || "Something went wrong");
+        }
+        return data ?? null;
+      } catch (error) {
+        throw new Error(error.message);
+      }
+    },
+    onSuccess: () => {
+      toast.success("Notification deleted successfully");
+      queryClient.invalidateQueries({ queryKey: ["notifications"] });
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
+
+  const handleDeleteNotification = (id) => {
+    singleNotification(id);
   };
 
   return (
@@ -60,7 +105,10 @@ const NotificationPage = () => {
           <div className="text-center p-4 font-bold">No notifications 🤔</div>
         )}
         {notifications?.map((notification) => (
-          <div className="border-b border-gray-700" key={notification._id}>
+          <div
+            className="flex justify-between items-center border-b border-gray-700"
+            key={notification._id}
+          >
             <div className="flex gap-2 p-4">
               {notification.type === "follow" && (
                 <FaUser className="w-7 h-7 text-primary" />
@@ -68,7 +116,10 @@ const NotificationPage = () => {
               {notification.type === "like" && (
                 <FaHeart className="w-7 h-7 text-red-500" />
               )}
-              <Link to={`/profile/${notification.from.username}`}>
+              <Link
+                to={`/profile/${notification.from.username}`}
+                className="flex items-center gap-2"
+              >
                 <div className="avatar">
                   <div className="w-8 rounded-full">
                     <img
@@ -76,6 +127,7 @@ const NotificationPage = () => {
                         notification.from.profileImg ||
                         "/avatar-placeholder.png"
                       }
+                      alt="profile"
                     />
                   </div>
                 </div>
@@ -89,6 +141,12 @@ const NotificationPage = () => {
                 </div>
               </Link>
             </div>
+            <button
+              className="text-red-500 hover:underline px-4"
+              onClick={() => handleDeleteNotification(notification._id)}
+            >
+              <FaTrashAlt className="w-5 h-5" /> 
+            </button>
           </div>
         ))}
       </div>
